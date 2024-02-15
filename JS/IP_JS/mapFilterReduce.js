4db8174f207186a173e79174b18050fdd2063430
Vedavyas <vedavyasjoshi121@gmail.com>
2024-02-16 00:26:02 +0530

    map filter reduce

---
+++ JS/IP_JS/mapFilterReduce.js
@@ -0,0 +1,46 @@
+const nums = [1, 2, 3, 4];
+
+// Map
+Array.prototype.myMap = function (cb) {
+  let res = [];
+  for (let i = 0; i < this.length; i++) {
+    res.push(cb(this[i], i, this));
+  }
+  return res;
+};
+const multiplyThree = nums.myMap((num, i, arr) => {
+  return num * 3 + i;
+});
+console.log(multiplyThree);
+
+// Filter
+Array.prototype.myFilter = function (cb) {
+  let res = [];
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i], i, this)) res.push(this[i]);
+  }
+  return res;
+};
+const moreThanTwo = nums.myFilter((num, i, arr) => {
+  return num > 2;
+});
+console.log(moreThanTwo);
+
+// Reduce
+// If acc is not initialized, it takes the first value, if arr is empty then err
+Array.prototype.myReduce = function (cb, initialValue) {
+  let acc = initialValue;
+  for (let i = 0; i < this.length; i++) {
+    acc = acc ? cb(acc, this[i], i, this) : this[i];
+  }
+  return acc;
+};
+const total = nums.myReduce((acc, curr, i, arr) => {
+  return acc + curr;
+}, 0);
+console.log(total);
+
+// map vs forEach => map returns new Array, forEach doesnt. 
+// We can chain other functions on each other, map, filter, reduce. but not on forEach
+
+
